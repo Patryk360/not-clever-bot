cbd6e607664c14d103af6e8759d7749496a5af7d
Patryk360 <nexigamingyt@gmail.com>
2025-03-09 09:54:18 +0100

    up

--- api/js/register.js
+++ api/js/register.js
@@ -1,13 +1,14 @@
 "use strict";
-$(document).ready(function() {
-    $("#register").click(function() {
+$(document).ready(() => {
+    $("#register").click(() => {
         $("#progress-bar").html(`<div class="progress" id="progress-bar-delete"><div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style="width: 100%"></div></div>`);
         const data = {
             username: $("#usernameVal").val(),
             email: $("#emailVal").val(),
             password: $("#passwordVal").val(),
+            retypePassword: $("#retypePasswordVal").val()
         };
-        $.post("/dashboard/register", data).done(function(done) {
+        $.post("/dashboard/register", data).done(() => {
             $("#progress-bar-delete").remove();
         });
     });
--- database/connect.js
+++ database/connect.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
     connect: async (rethinkdb) => {
         const connection = await rethinkdb.connect({
-            host: "localhost",
+            host: "130.61.95.106",
             port: 28015,
             db: "NotCleverBot",
             password: process.env.PASSWORD
